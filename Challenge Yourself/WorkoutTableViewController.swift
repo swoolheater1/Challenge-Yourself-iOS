8d9cb719ebb3690366016026541438624451feef
swoolheater1 <sirangwool@gmail.com>
2019-01-06 17:57:20 -0500

    Created full workout in loadExercise method

--- Challenge Yourself/WorkoutTableViewController.swift
+++ Challenge Yourself/WorkoutTableViewController.swift
@@ -74,20 +74,52 @@ class WorkoutTableViewController: UITableViewController {
     // MARK: Private Methods
     
     private func loadExercises() {
-        guard let exercise1 = Exercise(name: "1) PUSH UPS!", youtubeID: "r25IWquxe9s") else {
+        guard let exercise1 = Exercise(name: "Mountain Climbers ~ 1 min", youtubeID: "fBZHkGT0W5Y") else {
             fatalError("Unable to instantiate exercise1")
         }
-        guard let exercise2 = Exercise(name: "2) SQUAT", youtubeID: "r25IWquxe9s") else {
+        guard let exercise2 = Exercise(name: "Forearm Plank ~ 1 min", youtubeID: "Zy78sjo1TZE") else {
             fatalError("Unable to instantiate exersice2")
         }
-        guard let exercise3 = Exercise(name: "3) RUN", youtubeID: "r25IWquxe9s") else {
+        guard let exercise3 = Exercise(name: "20 Sit Ups", youtubeID: "1fbU_MkV7NE") else {
             fatalError("Unable to instantiate exersice2")
         }
-        guard let exercise4 = Exercise(name: "4) CRUNCH", youtubeID: "r25IWquxe9s") else {
+        guard let exercise4 = Exercise(name: "20 Push Ups", youtubeID: "eFOSh8vpd6I") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise5 = Exercise(name: "20 Squats", youtubeID: "aclHkVaku9U") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise6 = Exercise(name: "20 Tricep Dips", youtubeID: "pnt7HBOvUj8") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise7 = Exercise(name: "Burpees ~ 1 min", youtubeID: "dZgVxmf6jkA") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise8 = Exercise(name: "20 Lunges", youtubeID: "QOVaHwm-Q6U") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise9 = Exercise(name: "20 Leg Lifts", youtubeID: "JB2oyawG9KI") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise10 = Exercise(name: "20 Military Push Up", youtubeID: "cnOS4D_snr8") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise11 = Exercise(name: "20 Glute Bridges", youtubeID: "k4BQruxcpxw") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise12 = Exercise(name: "Superman ~ 30 sec X2", youtubeID: "VUT1RHyMEuc") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise13 = Exercise(name: "High Plank ~ 1 min", youtubeID: "5znHfRpjczc") else {
+            fatalError("Unable to instantiate exersice2")
+        }
+        guard let exercise14 = Exercise(name: "Stretch ~ 5 min", youtubeID: "2L2lnxIcNmo") else {
             fatalError("Unable to instantiate exersice2")
         }
         
-        exercises += [exercise1, exercise2, exercise3, exercise4]
+        exercises += [exercise1, exercise2, exercise3, exercise4, exercise5,
+                      exercise6, exercise7, exercise8, exercise9, exercise10,
+                      exercise11, exercise12, exercise13, exercise14]
     }
 
 }
